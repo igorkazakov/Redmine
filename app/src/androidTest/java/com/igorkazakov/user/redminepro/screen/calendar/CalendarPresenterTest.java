5ba4e19cd782d0973a4feb4849875c34b71497d7
IGOR_KAZAKOV <ikazakov@oggettoweb.com>
2018-05-22 23:02:11 +0300

    fix calendar test

--- app/src/androidTest/java/com/igorkazakov/user/redminepro/screen/calendar/CalendarPresenterTest.java
+++ app/src/androidTest/java/com/igorkazakov/user/redminepro/screen/calendar/CalendarPresenterTest.java
@@ -8,14 +8,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -37,11 +44,9 @@ public class CalendarPresenterTest {
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        calendarPresenter = new CalendarPresenter(repository);
-        when(repository.getCalendarDaysForYear())
-               .thenReturn(Observable.fromArray(new ArrayList<>()));
-        //ApiException exception = new ApiException("error", 9999);
-        // when(repository.getCalendarDaysForYear()).thenReturn(Observable.error(exception));
+        calendarPresenter = spy(new CalendarPresenter(repository));
+        Mockito.doNothing().when(calendarPresenter).onFirstViewAttach();
+
         calendarPresenter.attachView(calendarView);
     }
 
